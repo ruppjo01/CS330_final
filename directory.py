c0cdca690939fa2809e3489b07577491f9ac8e7b
Josh Rupprecht <ruppjo01@luther.edu>
2016-05-03 11:55:03 -0500

    basic bootstrap templating

---
+++ directory.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+app.debug = True
+
+Bootstrap(app)
+
+@app.route('/')
+def directory():
+    return render_template('index.html')
+
+@app.route('/student_map')
+def map():
+    return render_template('map.html')
+
+@app.route('/pictures')
+def pictures():
+    return render_template('pictures.html')
+
+if __name__ == '__main__':
+    app.run()
