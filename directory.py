61666898f96a4249d0632a38d381fde385aa6ace
Josh Rupprecht <ruppjo01@luther.edu>
2016-05-05 12:35:43 -0500

    beginning to implement WTForms on the main page

--- directory.py
+++ directory.py
@@ -1,14 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 from flask_bootstrap import Bootstrap
+from wtforms import Form, BooleanField, TextField, validators, SubmitField, RadioField, SelectField
+from flask_wtf import Form
 
 app = Flask(__name__)
 app.debug = True
+app.secret_key = 'development key'
 
 Bootstrap(app)
 
-@app.route('/')
+class StudentSearch(Form):
+    options = RadioField('Search By',[validators.DataRequired()],choices=[('Name','Name'),('Major','Major'),('Minor','Minor'),('Res','Luther Residence'),('Hometown','Hometown'),('State','Home State')])
+    field = TextField([validators.DataRequired()])
+
+    submit = SubmitField('Search')
+
+@app.route('/', methods=['GET','POST'])
 def directory():
-    return render_template('index.html')
+    form = StudentSearch()
+
+    if request.method == 'POST':
+        if form.validate() == False:
+            flash('At least one parameter must be given')
+            return render_template('index.html', form = form)
+        else:
+            return render_template('results.html')
+    elif request.method == 'GET':
+        return render_template('index.html', form = form)
+
 
 @app.route('/student_map')
 def map():
