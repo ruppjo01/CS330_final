fb6005b624880d64e0fb288696d62de986a6f60c
Josh Rupprecht <ruppjo01@luther.edu>
2016-05-05 20:51:46 -0500

    trying to get it to work

--- directory.py
+++ directory.py
@@ -2,12 +2,27 @@ from flask import Flask, render_template, request, flash
 from flask_bootstrap import Bootstrap
 from wtforms import Form, BooleanField, TextField, validators, SubmitField, RadioField, SelectField
 from flask_wtf import Form
+from flask_sqlalchemy import SQLAlchemy
+
+from wtforms_sqlalchemy.orm import model_form
 
 app = Flask(__name__)
 app.debug = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
 app.secret_key = 'development key'
 
+
 Bootstrap(app)
+
+
+
+class names(db.Model):
+    __tablename__ = 'names'
+    name_id = db.Column(db.Integer, primary_key=True)
+    first = db.Column(db.String)
+    last = db.Column(db.String)
 
 class StudentSearch(Form):
     options = RadioField('Search By',[validators.DataRequired()],choices=[('Name','Name'),('Major','Major'),('Minor','Minor'),('Res','Luther Residence'),('Hometown','Hometown'),('State','Home State')])
@@ -24,9 +39,20 @@ def directory():
             flash('At least one parameter must be given')
             return render_template('index.html', form = form)
         else:
-            return render_template('results.html')
+            if form.options.data == 'Name':
+                name = form.field.data.split()
+                first_name = name[0]
+                last_name = name[1]
+                info = db.session.query(names).filter_by(first = first_name).filter_by(last = last_name)
+                results = []
+                for i in info:
+                    results.append(i)
+
+
+            return render_template('results.html', info = results)
     elif request.method == 'GET':
         return render_template('index.html', form = form)
+
 
 
 @app.route('/student_map')
